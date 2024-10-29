1f8e72adfd0f337cc4e7cadd3f8a5f167a9a78a3
DKCL1989 <devilee1989@gmail.com>
2024-10-28 21:55:04 -0400

    upload the password

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "ATLASDB":"mongodb+srv://<username>:<password>@<hostname>/<database>?retryWrites=true&w=majority"
+    "ATLASDB":"mongodb+srv://dkclee:okOf89VEks8kM8G2@clustercomp229.ul6nj.mongodb.net/?retryWrites=true&w=majority&appName=ClusterCOMP229"
 }
